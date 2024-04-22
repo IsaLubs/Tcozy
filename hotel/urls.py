7eab83a039b5a247a15c6cc0a97bd478e03b5c38
Isalub <147058041+IsaLubs@users.noreply.github.com>
2024-04-22 11:43:05 +0200

    Update urls.py

--- hotel/urls.py
+++ hotel/urls.py
@@ -5,6 +5,7 @@ app_name = "hotel"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about_us", views.about_us, name="about_us"),
     path("detail/<slug:slug>/", views.hotel_detail, name="detail"),
     path("detail/<slug:slug>/room-type/<slug:rt_slug>/", views.room_type_detail, name="room_type_detail"),
     path("selected_rooms/", views.selected_rooms, name="selected_rooms"),
